ce0db0f9b2b8dfe0a548f56951f61d2c261d374a
witiftydev <wit.ifty.dev@gmail.com>
2026-06-21 09:53:19 +0600

    ✅initially

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
 import React from "react";
 
 const page = () => {
-  return <div>Demo Project to check deploy opn firebase</div>;
+  return (
+    <div>
+      Demo Project to check deploy opn firebase <br />
+      <Link href="/about" className="text-blue-500 hover:underline">
+        About Us
+      </Link>
+    </div>
+  );
 };
 
 export default page;
